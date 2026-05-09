06bb203d2472a5cc722a9d7d1073db64f80547a9
Juleczka-jpg <nowjulia12@gmail.com>
2026-05-09 10:11:42 +0200

    Update MainPage.xaml.cs

--- MauiApp7/MainPage.xaml.cs
+++ MauiApp7/MainPage.xaml.cs
@@ -13,18 +13,14 @@ namespace MauiApp7
 
         private async void OnGetRatesClicked(object sender, EventArgs e)
         {
-            // W MAUI HttpClient zazwyczaj żyje przez cały czas działania aplikacji
             using var client = new HttpClient();
             var service = new CurrencyService(client);
 
-            // Pobieranie danych
             double usd = await service.GetRateAsync("usd");
             double eur = await service.GetRateAsync("eur");
 
-            // Wypisanie do debuggera (zgodnie z zadaniem)
             Debug.WriteLine($"[DEBUG] USD: {usd}, EUR: {eur}");
 
-            // Wyświetlenie na ekranie (dla użytkownika)
             if (usd > 0 && eur > 0)
             {
                 ResultLabel.Text = $"USD: {usd:F4} PLN\nEUR: {eur:F4} PLN";
